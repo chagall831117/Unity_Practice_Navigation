436ea76dab49c8ff7ee5b3bcccd2397a184598c1
chagall831117 <chagall831117@users.noreply.github.com>
2020-02-01 16:53:20 +0800

    還沒有做好停止動畫

---
+++ UnityNavigation/Assets/RunAway.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RunAway : MonoBehaviour
+{
+    public Transform[] Locations;//隨機位置
+    public NavMeshAgent Nav; //領航員
+    public Animator Ani; //動畫控制器
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ani = GetComponent<Animator>();
+        Nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomRun();
+    }
+    public void RandomRun()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Nav.SetDestination(Locations[Random.Range(0, Locations.Length)].position);
+            Ani.SetBool("跑步開關", true);
+            Invoke("RunAni", 3f);
+        }
+    }
+    public void RunAni()
+    {
+        Ani.SetBool("跑步開關", false);
+    }
+}
